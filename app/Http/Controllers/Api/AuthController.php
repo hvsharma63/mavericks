983b8040b039a6578395eae1aedf9f2f13723347
Harshvardhan Sharma <hvsharma63@gmail.com>
2020-12-08 13:54:08 +0530

    [WIP] Debugging

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,24 +5,28 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class AuthController extends Controller
 {
     //
     public function login(Request $request)
     {
-        dd($request->all());
+        try {
+            if (auth()->attempt($data)) {
+                $token = auth()->user()->createToken(sha1(time()))->accessToken;
+                return response()->json(['token' => $token], 200);
+            } else {
+                return response()->json(['error' => 'Unauthorised'], 401);
+            }
+        } catch (\Exception $e) {
+            throw new HttpException(500, $e->getMessage());
+        }
         $data = [
             'email' => $request->email,
             'password' => $request->password
         ];
  
-        if (auth()->attempt($data)) {
-            $token = auth()->user()->createToken(sha1(time()))->accessToken;
-            return response()->json(['token' => $token], 200);
-        } else {
-            return response()->json(['error' => 'Unauthorised'], 401);
-        }
     }
 
     public function logout() {
